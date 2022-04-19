d3643bb7a6ffc8a23b49a935ce173627bff99bd3
Justus Tumacder <justusft@gmail.com>
2022-04-19 13:20:50 +0800

    Add react

---
+++ client/src/app/components/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export function App({}: Props) {
+  return <div>App</div>;
+}
--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,1 +1,5 @@
-console.log("hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#app"));
