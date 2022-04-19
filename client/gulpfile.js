32d1f9c69aa823dc3638c6d2982b45849a752156
Justus Tumacder <justusft@gmail.com>
2022-04-19 10:12:51 +0800

    Add wasm to gulpfile

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,11 +1,14 @@
+const exec = require("child_process").exec;
 const gulp = require("gulp");
 
-function testTask(cb) {
-  cb();
+function wasm() {
+  return exec(
+    "cd vendor/wormhole-william && GOOS=js GOARCH=wasm go build -o ../../dist/wormhole.wasm ./wasm/module"
+  );
 }
 
 function watch() {
-  gulp.watch("src/*.html", testTask);
+  gulp.watch("vendor/wormhole-william/**/*.go", wasm);
 }
 
 exports.default = watch;
