20413abe21860f15fac20aec30781a1595fb5521
Justus Tumacder <justusft@gmail.com>
2022-05-25 16:29:36 +0800

    Add client-e2e service

---
+++ client-e2e/test/wdio.conf.ts
@@ -0,0 +1,38 @@
+import type { Options } from "@wdio/types";
+
+export const config: Options.Testrunner = {
+  hostname: "selenium-hub",
+  autoCompileOpts: {
+    autoCompile: true,
+    tsNodeOpts: {
+      transpileOnly: true,
+      project: "test/tsconfig.json",
+    },
+  },
+  specs: ["./test/specs/**/*.ts"],
+  maxInstances: 10,
+  capabilities: [
+    {
+      browserName: "chrome",
+      acceptInsecureCerts: true,
+    },
+    {
+      browserName: "firefox",
+    },
+    {
+      browserName: "MicrosoftEdge",
+    },
+  ],
+  logLevel: "info",
+  bail: 0,
+  baseUrl: "http://localhost",
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  framework: "mocha",
+  reporters: ["spec"],
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
+};
