811a5f9d00c7a129dcc43ed74e17a0ef248e5210
Justus Tumacder <justusft@gmail.com>
2022-05-25 16:39:43 +0800

    Connect client to client-e2e

--- client-e2e/test/specs/example.e2e.ts
+++ client-e2e/test/specs/example.e2e.ts
@@ -1,15 +1,8 @@
-import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import * as Page from "../pageobjects/page";
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
-    });
+describe("My Login application", () => {
+  it("should login with valid credentials", async () => {
+    await Page.open();
+    await expect($("[data-testid=send-page-container]")).toBeExisting();
+  });
 });
-
-
