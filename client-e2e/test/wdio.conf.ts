1cfff38c614d32d30a50fad1317417261dc6dd36
Justus Tumacder <justusft@gmail.com>
2022-06-16 11:04:24 +0800

    Clean up wdio.conf.ts

--- client-e2e/test/wdio.conf.ts
+++ client-e2e/test/wdio.conf.ts
@@ -1,31 +1,8 @@
 import type { Options } from "@wdio/types";
 import * as fs from "fs";
 import * as fsExtra from "fs-extra";
-import * as path from "path";
 
-// global.downloadDir = path.join(__dirname, "tempDownload");
 global.downloadDir = "/tmp/test-downloads";
-
-// Pulled from https://gist.github.com/tkihira/2367067
-// this could be moved to a separate file if we wanted
-function rmdir(dir: string) {
-  var list = fs.readdirSync(dir);
-  for (var i = 0; i < list.length; i++) {
-    var filename = path.join(dir, list[i]);
-    var stat = fs.statSync(filename);
-
-    if (filename == "." || filename == "..") {
-      // pass these files
-    } else if (stat.isDirectory()) {
-      // rmdir recursively
-      rmdir(filename);
-    } else {
-      // rm fiilename
-      fs.unlinkSync(filename);
-    }
-  }
-  fs.rmdirSync(dir);
-}
 
 export const config: Options.Testrunner = {
   hostname: "selenium-hub",
@@ -50,12 +27,12 @@ export const config: Options.Testrunner = {
         },
       },
     },
-    // {
-    //   browserName: "firefox",
-    // },
-    // {
-    //   browserName: "MicrosoftEdge",
-    // },
+    {
+      browserName: "firefox",
+    },
+    {
+      browserName: "MicrosoftEdge",
+    },
   ],
   logLevel: "info",
   bail: 0,
@@ -67,8 +44,7 @@ export const config: Options.Testrunner = {
   reporters: ["spec"],
   mochaOpts: {
     ui: "bdd",
-    // timeout: 60000,
-    timeout: 24 * 60 * 60 * 1000,
+    timeout: 60000,
   },
   onPrepare: function (_config, _capabilities) {
     fs.chownSync(global.downloadDir, 1200, 1201);
