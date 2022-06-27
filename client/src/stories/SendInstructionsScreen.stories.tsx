4c735d39ea3dcc0656af334a0e6f2a8d1ab9d320
Justus Tumacder <justusft@gmail.com>
2022-06-27 15:42:43 +0800

    Add SendInstructionsScreen story

---
+++ client/src/stories/SendInstructionsScreen.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import AppTemplate from "../app/components/AppTemplate";
+import { SendInstructionsScreenContent } from "../app/components/screens/send/SendInstructionsScreen";
+
+export default {
+  title: "screens/SendInstructionsScreenContent",
+  component: SendInstructionsScreenContent,
+  parameters: {
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof SendInstructionsScreenContent>;
+
+const Template: ComponentStory<typeof SendInstructionsScreenContent> = (
+  args
+) => (
+  <MemoryRouter initialEntries={["/"]}>
+    <AppTemplate>
+      <SendInstructionsScreenContent {...args} />
+    </AppTemplate>
+  </MemoryRouter>
+);
+
+export const Story = Template.bind({});
+Story.args = {
+  code: "7-guitarist-revenge",
+  copied: false,
+  onCopy: () => console.log("onCopy"),
+  onCancel: () => console.log("onCancel"),
+};
