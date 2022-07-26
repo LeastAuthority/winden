fc7333efb6d7bdb23b26f2ea0f3e01ad48941d59
Justus Tumacder <justusft@gmail.com>
2022-07-26 09:34:23 +0800

    Add codeinput error message tests

--- client/src/app/components/CodeInput.test.tsx
+++ client/src/app/components/CodeInput.test.tsx
@@ -182,4 +182,101 @@ describe("<CodeInput />", () => {
       });
     });
   });
+
+  describe("error message visibility", () => {
+    describe("the initial state", () => {
+      it("will not show the error", () => {
+        render(
+          <CodeInputProvider>
+            <CodeInput />
+          </CodeInputProvider>
+        );
+        const error = screen.getByTestId("code-error-message");
+        expect(error.textContent).toEqual("");
+      });
+    });
+
+    describe("when unfocused with an invalid code", () => {
+      it("will show the error", async () => {
+        const user = userEvent.setup();
+        render(
+          <CodeInputProvider>
+            <CodeInput />
+          </CodeInputProvider>
+        );
+        const container = screen.getByTestId("code-input-container");
+        const input = screen.getByTestId("code-input");
+        const error = screen.getByTestId("code-error-message");
+        await user.type(input, "asdf");
+        await user.click(container);
+        expect(error.textContent).not.toEqual("");
+      });
+    });
+
+    describe("when unfocused with valid code", () => {
+      it("will not show the error", async () => {
+        const user = userEvent.setup();
+        render(
+          <CodeInputProvider>
+            <CodeInput />
+          </CodeInputProvider>
+        );
+        const container = screen.getByTestId("code-input-container");
+        const input = screen.getByTestId("code-input");
+        const error = screen.getByTestId("code-error-message");
+        await user.type(input, "7-guitarist-revenge");
+        await user.click(container);
+        expect(error.textContent).toEqual("");
+      });
+    });
+
+    describe("when focused with an invalid code", () => {
+      describe("the user hit enter", () => {
+        it("will show the error", async () => {
+          const user = userEvent.setup();
+          render(
+            <CodeInputProvider>
+              <CodeInput />
+            </CodeInputProvider>
+          );
+          const input = screen.getByTestId("code-input");
+          const error = screen.getByTestId("code-error-message");
+          await user.type(input, "asdf{Enter}");
+          expect(error.textContent).not.toEqual("");
+        });
+      });
+
+      describe("the user did not hit enter", () => {
+        it("will not show the error", async () => {
+          const user = userEvent.setup();
+          render(
+            <CodeInputProvider>
+              <CodeInput />
+            </CodeInputProvider>
+          );
+          const input = screen.getByTestId("code-input");
+          const error = screen.getByTestId("code-error-message");
+          await user.type(input, "asdf");
+          expect(error.textContent).toEqual("");
+        });
+      });
+
+      describe("the user hit enter then another key", () => {
+        it("will show the error then hide it", async () => {
+          const user = userEvent.setup();
+          render(
+            <CodeInputProvider>
+              <CodeInput />
+            </CodeInputProvider>
+          );
+          const input = screen.getByTestId("code-input");
+          const error = screen.getByTestId("code-error-message");
+          await user.type(input, "{Enter}");
+          expect(error.textContent).not.toEqual("");
+          await user.type(input, "asdf");
+          expect(error.textContent).toEqual("");
+        });
+      });
+    });
+  });
 });
