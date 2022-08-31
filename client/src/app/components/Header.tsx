4987818639627e8486f10c639314a0fcdbc32445
Justus Tumacder <justusft@gmail.com>
2022-08-31 13:20:38 +0800

    Change casing for send/receive buttons

--- client/src/app/components/Header.tsx
+++ client/src/app/components/Header.tsx
@@ -47,7 +47,7 @@ export default function Header() {
               }
             }}
           >
-            RECEIVE
+            Receive
           </Button>
         ) : (
           <Button
@@ -64,7 +64,7 @@ export default function Header() {
               }
             }}
           >
-            SEND
+            Send
           </Button>
         )}
       </Group>
