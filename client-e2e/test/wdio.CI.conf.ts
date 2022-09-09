dac26d9b9e8f060cdee3a3704e12b6ee048d181c
Donatas Puidokas <donatas@leastauthority.com>
2022-09-09 11:20:37 +0300

    adding html reporter

--- client-e2e/test/wdio.CI.conf.ts
+++ client-e2e/test/wdio.CI.conf.ts
@@ -1,7 +1,14 @@
+// config for Github workflow to run
+
 import type { Options } from "@wdio/types";
 import { execSync } from "child_process";
 import * as fs from "fs";
 import * as fsExtra from "fs-extra";
+
+import {ReportAggregator, HtmlReporter} from 'wdio-html-nice-reporter';
+import commands from "@rpii/wdio-commands";
+let reportAggregator: ReportAggregator;
+var log4js = require("log4js");
 
 global.downloadDirBrowser = "/home/seluser/downloads";
 global.downloadDir = "/home/node/downloads";
@@ -15,7 +22,8 @@ export const config: Options.Testrunner = {
       project: "test/tsconfig.json",
     },
   },
-  specs: ["./test/specs/**/*.ts"],
+  //specs: ["./test/specs/**/*.ts"],
+  specs: ["./test/specs/receive.ts"],
   maxInstances: 1,
   capabilities: [
     {
@@ -38,7 +46,7 @@ export const config: Options.Testrunner = {
           "browser.download.folderList": 2,
         },
       },
-    },
+    },*//*
     {
       browserName: "MicrosoftEdge",
       "ms:edgeOptions": {
@@ -58,15 +66,58 @@ export const config: Options.Testrunner = {
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
   framework: "mocha",
-  reporters: ["spec"],
+  //reporters: ["spec"],
   mochaOpts: {
     ui: "bdd",
     timeout: 60000,
   },
-  onPrepare: function (_config, _capabilities) {
+  onPrepare: function (config, capabilities) {
     execSync("/usr/src/app/scripts/generate-CI-test-files.sh");
+    reportAggregator = new ReportAggregator({
+            outputDir: './reports/html-reports/',
+            filename: 'master-report.html',
+            reportTitle: 'Master Report',
+            browserName: capabilities.browserName,
+            collapseTests: true
+          });
+        reportAggregator.clean();
+  },
+  before: function (capabilities, specs) {
+
+        //@ts-ignore
+        commands.addCommands(driver);
   },
   beforeTest: function () {
     fsExtra.emptyDirSync(global.downloadDir);
+  },
+  afterTest: function (test: any, context: any, result: any) {
+        // if test passed, ignore, else take and save screenshot.
+        if (result.passed) {
+            return;
+        }
+        //@ts-ignore
+        driver.logScreenshot(String.Format("Test Ended in {0}", result.error.stack));
+  },
+  reporters: ['spec',
+        ["html-nice", {
+            outputDir: './reports/html-reports/',
+            filename: 'report.html',
+            reportTitle: 'E2E tests report',
+            linkScreenshots: true,
+            //to show the report in a browser when done
+            showInBrowser: false,
+            collapseTests: false,
+            //to turn on screenshots after every test
+            useOnAfterCommandForScreenshot: false,
+
+            //to initialize the logger
+            LOG: log4js.getLogger("default")
+        }
+        ]
+  ],
+  onComplete: function(exitCode, config, capabilities, results) {
+        (async () => {
+            await reportAggregator.createReport();
+        })();
   },
 };
