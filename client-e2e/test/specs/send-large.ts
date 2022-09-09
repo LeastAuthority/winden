105a0356ad3266c42426d98e516bbc32524537b4
Donatas Puidokas <donatas@leastauthority.com>
2022-09-09 14:25:16 +0300

    Fix workflow and change tests

--- client-e2e/test/specs/send-large.ts
+++ client-e2e/test/specs/send-large.ts
@@ -43,41 +43,25 @@ async function testTransferFailure(fileName: string, timeout?: number) {
 }
 
 describe("Send flow", () => {
-  describe("on uploading a single file", () => {
-    it("will generate a code", async () => {
-      await Page.open();
-      await Page.uploadFiles("/usr/src/app/test/files/hello-world.txt");
 
-      const input = await $("input[readonly='']");
-      const re = new RegExp(
-        `^http://${process.env.HOST_IP}:8080/#/\\d+-\\w+-\\w+$`
-      );
-      await expect(input).toHaveValue(re);
-
-      const content = await $("main");
-      await expect(content).toHaveTextContaining("hello-world.txt");
+  describe("when uploading a file with the size of 300MB", () => {
+    it.skip("will tell the user that the file is too large", async () => {
+      await testTransferFailure("sizes/300MB");
     });
   });
 
-  describe("when uploading a file less than 1MB", () => {
-    it("will transfer successfully", async () => {
-      await testTransferSuccess("hello-world.txt");
+  describe("when uploading a file with the size of 4.2GB", () => {
+    it.skip("will tell the user that the file is too large", async function () {
+      this.timeout(120000);
+      await testTransferFailure("sizes/4.2GB");
     });
   });
 
-  describe("when uploading a file with the size of 20MB", () => {
-    it("will transfer successfully", async () => {
-      await testTransferSuccess("sizes/20MB");
+  describe("when uploading a file with the size of 4.3GB", () => {
+    it.skip("will tell the user that the file is too large", async function () {
+      this.timeout(120000);
+      await testTransferFailure("sizes/4.3GB");
     });
   });
 
-  describe("when a sender tries to send the same file twice", () => {
-    it("will successfully upload the file both times", async () => {
-      await Page.open();
-      await Page.uploadFiles("/usr/src/app/test/files/hello-world.txt");
-      await (await $("button*=Cancel")).click();
-      await Page.uploadFiles("/usr/src/app/test/files/hello-world.txt");
-      await expect(await $("main")).toHaveTextContaining("Ready to send!");
-    });
-  });
 });
