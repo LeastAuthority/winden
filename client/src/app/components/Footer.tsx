00fa749749e65fe3bc70b637e1a326c088cf4135
Justus Tumacder <JustusFT@gmail.com>
2022-09-29 05:11:45 +0800

    Add new responsive header and footer (#29)
    * Add new footer for wide viewports
    
    * Add Navbar for mobile viewport
    
    * Add Navbar to App
    
    * Close navbar when clicked on link
    
    * Add motto to footer for mobile screens
    
    * Lower weight for motto
    
    * Add Tagline component
    
    * Add missing regular font weight
    
    * Increase left padding in navbar
    
    * Remove hardcoded width for navbar
    
    * Fix content overflowing on footer
    
    * Refactor Footer
    
    * Update and refactor header design
    
    * Remove AppShell theming
    
    * Make send/receive text smaller on mobile
    
    * Set min width to 330
    
    * Fix button height for mobile header
    
    * Fix content full height issue
    
    * Fix centering of hamburger menu
    
    * Fix dropzone min height issue
    
    * Add smaller logo on super narrow screens
    
    * Increase top padding of header for narrow screens

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -1,47 +1,66 @@
 import {
   Anchor,
-  Center,
+  Box,
   createStyles,
+  Divider,
   Group,
   Image,
   Space,
   Stack,
   Text,
 } from "@mantine/core";
-import classNames from "classnames";
+import { useViewportSize } from "@mantine/hooks";
 import React from "react";
 import { Link } from "react-router-dom";
 
+const BREAKPOINT_NARROW_MOBILE_PX = 390;
+const BREAKPOINT_MOBILE_PX = 590;
+const BREAKPOINT_DESKTOP_NARROW_PX = 850;
+const BREAKPOINT_DESKTOP_PX = 1050;
+
 const useStyles = createStyles((theme) => ({
-  laMadeByTextLarge: {
-    [`@media (min-width: ${theme.breakpoints.sm}px) and (max-width: ${theme.breakpoints.md}px)`]:
-      {
-        display: "none",
-      },
-  },
-  laMadeByTextSmall: {
-    // [`@media (max-width: ${theme.breakpoints.xs}px)`]: {
-    //   display: "none",
-    // },
-  },
-  footerLarge: {
-    [`@media (max-width: ${theme.breakpoints.sm - 1}px)`]: {
-      display: "none",
-    },
-  },
-  footerSmall: {
-    [`@media (min-width: ${theme.breakpoints.sm}px)`]: {
-      display: "none",
-    },
+  feedbackLink: {
+    backgroundColor: theme.colors.blue,
+    padding: "5.5px 11px",
+    borderRadius: "5px",
   },
 }));
 
-function Links() {
+function Tagline() {
   return (
-    <>
+    <Text size="sm" color="dark-grey" weight={400}>
+      Made with love for privacy by
+    </Text>
+  );
+}
+
+function Logo() {
+  return <Image width="auto" height={30} fit="contain" src="/la-logo.svg" />;
+}
+
+function Links() {
+  const { classes } = useStyles();
+
+  return (
+    <Group spacing={16}>
+      <Anchor
+        component={Link}
+        className={classes.feedbackLink}
+        to="/about"
+        size="sm"
+        color="black"
+        weight={600}
+      >
+        Feedback
+      </Anchor>
+      <Anchor component={Link} to="/about" size="sm" color="black" weight={600}>
+        About
+      </Anchor>
+      <Divider orientation="vertical" />
       <Anchor component={Link} to="/faq" size="sm" color="black" weight={600}>
         FAQ
       </Anchor>
+      <Divider orientation="vertical" />
       <Anchor
         component={Link}
         to="/privacy"
@@ -51,9 +70,21 @@ function Links() {
       >
         Privacy
       </Anchor>
-      <Anchor component={Link} to="/about" size="sm" color="black" weight={600}>
-        About Us
+      <Divider orientation="vertical" />
+      <Anchor component={Link} to="/terms" size="sm" color="black" weight={600}>
+        Terms
       </Anchor>
+      <Divider orientation="vertical" />
+      <Anchor
+        component={Link}
+        to="/for-business"
+        size="sm"
+        color="black"
+        weight={600}
+      >
+        For Business
+      </Anchor>
+      <Divider orientation="vertical" />
       <Anchor
         href="https://github.com/LeastAuthority/winden"
         target="_blank"
@@ -63,56 +94,59 @@ function Links() {
       >
         GitHub
       </Anchor>
-    </>
+    </Group>
   );
 }
 
-export default function Footer() {
-  const { classes } = useStyles();
+type Props = {};
 
-  return (
-    <>
-      <Space h="lg" />
-
-      <Group spacing={8} position="apart" className={classes.footerLarge}>
-        <Group position="apart" style={{ width: 380 }} ml={40}>
-          <Links />
-        </Group>
-        <div style={{ flex: 1 }} />
-        <Text
-          size="sm"
-          color="dark-grey"
-          className={classNames(classes.laMadeByTextLarge)}
-          weight={600}
-        >
-          Made with love for privacy by
-        </Text>
-        <div>
-          <Image width="auto" height={30} fit="contain" src="/la-logo.svg" />
-        </div>
-        <Space w={40} />
-      </Group>
-
-      <Stack className={classes.footerSmall}>
-        <Group position="center" spacing={60}>
-          <Links />
-        </Group>
-        <Center>
-          <Text
-            size="sm"
-            color="dark-grey"
-            className={classNames(classes.laMadeByTextSmall)}
-            weight="bold"
-          >
-            Made with love for privacy by
-          </Text>
-          <div>
-            <Image width="auto" height={30} fit="contain" src="/la-logo.svg" />
-          </div>
-        </Center>
+function FooterLayout(props: Props) {
+  const { width } = useViewportSize();
+  if (width < BREAKPOINT_NARROW_MOBILE_PX) {
+    return (
+      <Stack spacing={6} align="center">
+        <Tagline />
+        <Logo />
       </Stack>
+    );
+  } else if (width < BREAKPOINT_MOBILE_PX) {
+    return (
+      <Group spacing={8} position="center">
+        <Tagline />
+        <Logo />
+      </Group>
+    );
+  } else if (width < BREAKPOINT_DESKTOP_NARROW_PX) {
+    return (
+      <Stack spacing={6} align="center">
+        <Links />
+        <Group spacing={8}>
+          <Tagline />
+          <Logo />
+        </Group>
+      </Stack>
+    );
+  } else {
+    return (
+      <Group spacing={0}>
+        <Links />
+        <div style={{ flex: 1 }} />
+        <Group spacing={8}>
+          {width >= BREAKPOINT_DESKTOP_PX && <Tagline />}
+          <Logo />
+        </Group>
+      </Group>
+    );
+  }
+}
 
+export default function Footer() {
+  const { width } = useViewportSize();
+  return (
+    <Box px={width < BREAKPOINT_DESKTOP_NARROW_PX ? 0 : 40}>
       <Space h="lg" />
-    </>
+      <FooterLayout />
+      <Space h="lg" />
+    </Box>
   );
 }
