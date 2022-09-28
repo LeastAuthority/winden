61dd5f32f6f808af3b5b3a5590c9b90a63e5a542
Justus Tumacder <justusft@gmail.com>
2022-09-29 05:35:25 +0800

    Update formatting in FeedbackPage

--- client/src/app/components/pages/FeedbackPage.tsx
+++ client/src/app/components/pages/FeedbackPage.tsx
@@ -7,32 +7,22 @@ export default function FeedbackPage({}: Props) {
   return (
     <Content>
       <h1>Feedback</h1>
-      <p>
-        Loving Winden? Meh? Kind of?
-      </p>
+      <p>Loving Winden? Meh? Kind of?</p>
       <p>
         Regardless, we would <b>enjoy hearing from you</b>!
       </p>
       <p>
-        Please send us an email at <a href="mailto:feedback@winden.app">feedback@winden.app</a> with any thoughts you may have about using Winden.
+        Please send us an email at{" "}
+        <a href="mailto:feedback@winden.app">feedback@winden.app</a> with any
+        thoughts you may have about using Winden.
       </p>
-      <p>
-        For example:
-      </p>
+      <p>For example:</p>
       <ul>
-        <li>
-          What’s great?
-        </li>  
-        <li>
-          What do you find it useful for? 
-        </li>  
-        <li>
-          What’s missing?
-        </li>
+        <li>What’s great?</li>
+        <li>What do you find it useful for?</li>
+        <li>What’s missing?</li>
       </ul>
-      <p>
-        Your feedback helps us make Winden the best it can be.
-      </p>
+      <p>Your feedback helps us make Winden the best it can be.</p>
     </Content>
   );
 }
