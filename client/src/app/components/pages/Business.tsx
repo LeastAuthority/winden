1dfffdfd80f3d95cdc716d23a2f752a30e97c6f7
Justus Tumacder <justusft@gmail.com>
2022-09-29 05:34:54 +0800

    Update formatting in Business.tsx

--- client/src/app/components/pages/Business.tsx
+++ client/src/app/components/pages/Business.tsx
@@ -8,16 +8,25 @@ export default function BusinessPage({}: Props) {
     <Content>
       <h1>For Business</h1>
       <p>
-        <b>Interested in bringing Winden’s real-time connections to your organization?</b>
+        <b>
+          Interested in bringing Winden’s real-time connections to your
+          organization?
+        </b>
       </p>
       <p>
-        Winden can be used for easy and secure file transfers within your organization, or externally with other organizations, customers, or other relations.
+        Winden can be used for easy and secure file transfers within your
+        organization, or externally with other organizations, customers, or
+        other relations.
       </p>
       <p>
-        The technology behind Winden can also be used to easily and securely introduce devices to one another, without needing to create sign-ups or exchange identity information. 
+        The technology behind Winden can also be used to easily and securely
+        introduce devices to one another, without needing to create sign-ups or
+        exchange identity information.
       </p>
       <p>
-        If you would like to know about everything the technology is capable of, please contact us at <a href="mailto:business@winden.app">business@winden.app</a>.
+        If you would like to know about everything the technology is capable of,
+        please contact us at{" "}
+        <a href="mailto:business@winden.app">business@winden.app</a>.
       </p>
     </Content>
   );
