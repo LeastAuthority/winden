bad56d24f80a13ba140f42a77a84b4c60257b753
donpui <donpui@gmail.com>
2022-09-30 16:04:52 +0300

    Adjusted generation script

--- client-e2e/scripts/generate-CI-test-files.sh
+++ client-e2e/scripts/generate-CI-test-files.sh
@@ -2,5 +2,6 @@
 if [ ! -d "/usr/src/app/test/files/sizes" ]; then
   mkdir /usr/src/app/test/files/sizes
   cd /usr/src/app/test/files/sizes
+  dd if=/dev/zero of=5MB bs=5000000 count=1
   dd if=/dev/zero of=20MB bs=20000000 count=1
 fi
