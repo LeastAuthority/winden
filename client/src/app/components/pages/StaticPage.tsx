aa1265c9291e3f673d126f3a4a353cd52a00175f
Justus Tumacder <JustusFT@gmail.com>
2022-10-03 17:44:37 +0800

    Refactor static pages (#47)

---
+++ client/src/app/components/pages/StaticPage.tsx
@@ -0,0 +1,32 @@
+import about from "!raw-loader!./static/about.html";
+import business from "!raw-loader!./static/business.html";
+import faq from "!raw-loader!./static/faq.html";
+import feedback from "!raw-loader!./static/feedback.html";
+import privacy from "!raw-loader!./static/privacy.html";
+import terms from "!raw-loader!./static/terms.html";
+import { TypographyStylesProvider } from "@mantine/core";
+import React from "react";
+import Content from "../Content";
+
+const pages = {
+  about,
+  business,
+  faq,
+  feedback,
+  privacy,
+  terms,
+};
+
+type Props = {
+  page: keyof typeof pages;
+};
+
+export default function StaticPage(props: Props) {
+  return (
+    <Content>
+      <TypographyStylesProvider>
+        <div dangerouslySetInnerHTML={{ __html: pages[props.page] }}></div>
+      </TypographyStylesProvider>
+    </Content>
+  );
+}
