ab633609aa958227b3951b51c2c7b3102cf87d3d
Justus Tumacder <justusft@gmail.com>
2022-11-04 13:16:00 +0800

    Add external link icons

---
+++ client/src/app/components/pages/static/TermsPage.tsx
@@ -0,0 +1,345 @@
+import React from "react";
+import { ExternalLink } from "tabler-icons-react";
+import StaticPage from "../StaticPage";
+
+type Props = {};
+
+export default function TermsPage({}: Props) {
+  return (
+    <StaticPage>
+      <h1>GENERAL TERMS AND CONDITIONS (the "Terms")</h1>
+      <small>Last updated: September 27, 2022</small>
+      <br />
+      <br />
+      <p>
+        Thank you for choosing our service which allows people to transfer data
+        ("Service"). The Service is provided by Least Authority TFA GmbH,
+        Thaerstraße 28a, 10249 Berlin (<b>"Least Authority"</b>, <b>"we"</b> or{" "}
+        <b>"us"</b> or <b>"our"</b>). By using the Service, you conclude a
+        binding agreement with us under the following terms and conditions. The
+        Terms shall apply in the version current at the time of the first use of
+        the Service as a framework agreement (§ 305 (3) of the German Civil Code
+        (BGB)) and also to subsequent uses of the Service by the same person
+        without having to refer to them again:
+      </p>
+      <br />
+      <ol>
+        <li>
+          <b>Scope of the Service</b>
+
+          <ol>
+            <li>
+              The Service allows a person (<b>"Sender"</b>) to transfer data
+              through the Service to another person (<b>"Receiver"</b>). For
+              this purpose, we provide the Sender with a one-time code or other
+              means to enable another person’s access to the content. The Sender
+              may forward these means to the Receiver using any method the
+              Sender deems appropriate for this purpose. We do not forward the
+              means to the Receiver as part of our Service. The Receiver may use
+              the means to establish a one-time transfer of the content.
+            </li>
+            <li>
+              Both Sender and/or Receiver can use the Service without prior
+              registration.
+            </li>
+            <li>
+              Both sending and receiving data through the Service is a use of
+              the Service. You shall only use the Service in accordance with
+              these Terms.
+            </li>
+            <li>
+              We provide the Service "as is" and free of charge. These Terms do
+              not grant you any right or claim with respect to any form,
+              functionality and/or availability of the Service.
+            </li>
+            <li>
+              We especially reserve the right to alter, amend, suspend, or
+              discontinue the Service at any time at our own discretion.
+            </li>
+            <li>
+              It is technically possible to connect and use the backend servers
+              used by Winden with applications other than Winden. By using these
+              servers with other applications, you agree to be bound to these
+              same Terms.
+            </li>
+            <li>
+              We offer our services in support of international human rights,
+              including the rights to freedom of expression and privacy.
+            </li>
+          </ol>
+        </li>
+
+        <li>
+          <b>Own responsibility</b>
+
+          <ol>
+            <li>
+              You are fully and solely responsible for the content that you
+              transfer via our Service and the consequences of such transfer.
+            </li>
+            <li>
+              If the content that you transfer through the Service is subject to
+              potential rights of third parties, you hereby undertake to ensure
+              that you are authorized to transfer and use the content and that
+              you do not infringe any third party rights.
+            </li>
+            <li>
+              If you are a Sender, you must ensure that the transfer of content
+              is in compliance with applicable law. As a Sender you especially
+              ensure that the transfer complies with all applicable export
+              control regulations.
+            </li>
+          </ol>
+        </li>
+
+        <li>
+          <b>Prohibition of misuse</b>
+
+          <ol>
+            <li>
+              Misusing the Service or helping others to do so is strictly
+              prohibited. You therefore agree that you will not use the Service
+              in any harmful or malicious way. In particular, but not
+              exclusively, you shall not do one of the following:
+              <ol>
+                <li>transferring content that violates applicable law;</li>
+                <li>
+                  transferring content that depict the sexual exploitation of
+                  children and adolescents, extreme violence, promotion of
+                  terrorist acts,, as well as hatred or discrimination of
+                  persons or groups of persons;
+                </li>
+                <li>
+                  disrupting the Service or interfering with other users, e.g.
+                  by using viruses or any other forms of attacks;
+                </li>
+                <li>
+                  infringing data protection and/or the privacy and/or further
+                  rights of others, in particular their intellectual property
+                  rights;
+                </li>
+                <li>modifying or otherwise altering our Service;</li>
+                <li>
+                  circumventing or otherwise interfering with the security
+                  measures of our Service.
+                </li>
+              </ol>
+            </li>
+          </ol>
+        </li>
+
+        <li>
+          <b>Blocking or limiting</b>
+
+          <ol>
+            <li>
+              We hereby expressly reserve the right to block or limit access to
+              the Service for good cause at any time and without prior notice.
+            </li>
+            <li>
+              Good cause for blocking or limiting access to the Service shall in
+              particular be given in case of misuse of the Service and/or other
+              violations of these Terms.
+            </li>
+          </ol>
+        </li>
+
+        <li>
+          <b>No Responsibility of Last Authority for user content</b>
+
+          <ol>
+            <li>
+              If you transfer/receive content through the Service, this is
+              technically facilitated by a stream of data. We do not store any
+              content at our servers or systems.
+            </li>
+            <li>
+              You explicitly acknowledge that it is an inherent part of our
+              Service that we do not and cannot monitor or control the data
+              content that is transferred via the Service to you or other users.
+            </li>
+            <li>
+              Only the Sender has knowledge of the nature and type of content
+              that the Sender transfers via our Service. If a Sender forwards to
+              you means for receiving content and you are unsure about the
+              nature and/or type of this content, you may refrain from receiving
+              it at your own discretion.
+            </li>
+            <li>
+              If you receive content through the Service, it is at your own
+              risk. We do not bear any responsibility for the content which you
+              receive through the Service.
+            </li>
+          </ol>
+        </li>
+
+        <li>
+          <b>
+            Liability (<i>Haftung</i>)
+          </b>
+
+          <ol>
+            <li>
+              We are only liable to you as stated hereinafter.
+              <ol>
+                <li>
+                  We are liable for damages which we or our vicarious agents (
+                  <i>Erfüllungsgehilfen</i>) caused intentionally and grossly
+                  negligent or which we caused by violating a guarantee
+                  undertaken by us.
+                </li>
+                <li>
+                  We are further liable for damages which we caused by a
+                  slightly negligent breach of an essential obligation necessary
+                  for the performance of these Terms (
+                  <i>wesentliche Vertragspflichten</i>). In this case, our
+                  liability shall be limited to typical and foreseeable damages.
+                </li>
+              </ol>
+            </li>
+
+            <li>
+              A limitation period (<i>Verjährungsfrist</i>) of one year applies
+              to any liability claims against us. This begins at the end of the
+              year in which the claim arose and you became aware or should have
+              become aware of the circumstances giving rise to the claim.
+            </li>
+            <li>
+              Any limitation of our liability does not apply in the case of
+              gross negligence and intent, liability under the Product Liability
+              Act (<i>Produkthaftungsgesetz</i>) or in the case of damages to
+              the life, body and health of natural persons.
+            </li>
+            <li>
+              You agree that you shall be liable to us in accordance with the
+              provisions of statutory law.
+            </li>
+          </ol>
+        </li>
+
+        <li>
+          <b>Term; Termination</b>
+
+          <ol>
+            <li>
+              The Terms apply for each one-time use of the Service. Our
+              agreement for the use of the Service terminates automatically when
+              the transfer of content is completed or fails.
+            </li>
+            <li>
+              For clarity: If you commence another use of the Service, you are
+              again bound by the Terms.
+            </li>
+          </ol>
+        </li>
+
+        <li>
+          <b>Open Source Software</b>
+
+          <ol>
+            <li>
+              You acknowledge that the software which we use to provide the
+              Service is Open-Source-Software (<b>"OSS"</b>). You find a list of
+              the applicable OSS licenses, related copyright notices and the
+              source code under GitHub repository{" "}
+              <a
+                href="https://github.com/LeastAuthority/winden"
+                rel="noopener noreferrer"
+              >
+                https://github.com/LeastAuthority/winden
+                <ExternalLink size={16} />
+              </a>
+              .
+            </li>
+            <li>
+              The provisions in these Terms shall not limit your rights under
+              the applicable OSS licenses in any way.
+            </li>
+          </ol>
+        </li>
+
+        <li>
+          <b>Limitation of warranty</b>
+
+          <ol>
+            <li>
+              You agree with us that we do not assume any warranty (
+              <i>Gewährleistung</i>) for or in relation to the Service. Any and
+              all statutory warranty shall be excluded to the extent permissible
+              under applicable law.
+            </li>
+            <li>
+              For clarity but not as a limitation to the aforementioned
+              exclusion of warranty, we point you to the following:
+              <ol>
+                <li>
+                  We do especially not warrant any particular form,
+                  functionality and/or availability of the Service, neither for
+                  Senders nor for Receivers;
+                </li>
+                <li>
+                  If you are a Sender, we especially do not warrant that the
+                  Service is fit for any purpose or any of your specific
+                  requirements, that the Service can be used uninterrupted,
+                  timely or error-free.
+                </li>
+                <li>
+                  If you are a Receiver, we explicitly do not warrant that the
+                  content which you receive through the Service is complete,
+                  correct, lawful, error-free and/or virus-free.
+                </li>
+              </ol>
+            </li>
+          </ol>
+        </li>
+
+        <li>
+          <b>Choice of law and jurisdiction</b>
+
+          <ol>
+            <li>
+              These Terms and their interpretation and any non-contractual
+              obligations in connection with it are subject to German
+              substantive law. The UN Convention on Contracts for the
+              International Sale of Goods (CISG) shall not apply.
+            </li>
+            <li>
+              English language terms used in these Terms describe German legal
+              concepts only and shall not be interpreted by reference to any
+              meaning attributed to them in any jurisdiction other than Germany.
+              Where a German term has been inserted in brackets and/or italics
+              it alone (and not the English term to which relates) shall be
+              authoritative for the purpose of the interpretation of the
+              relevant term whenever it is used in these Terms.
+            </li>
+            <li>
+              Exclusive place of jurisdiction for all disputes regarding rights
+              and duties under this Agreement, including its validity shall be
+              Berlin, Germany unless you agree to these Terms in your capacity
+              as a natural person (and not on behalf of a legal entity). In the
+              latter case, the place of jurisdiction shall be determined based
+              on statutory law.
+            </li>
+          </ol>
+        </li>
+
+        <li>
+          <b>Severability</b>
+          <br />
+          Should one or more provisions of these Terms be or become invalid or
+          unenforceable in whole or in part, this shall not affect the validity
+          and enforceability of the remaining provisions of these Terms. In
+          place of any Standard Terms of Business (
+          <i>Allgemeine Geschäftsbedingungen</i>) which are invalid or not
+          incorporated in the Terms the statutory provisions shall apply (§ 306
+          (2) of the German Civil Code (BGB)). In all other cases, we shall
+          agree a valid provision to replace the invalid or unenforceable
+          provision which reflects as closely as possible the original economic
+          purpose, provided a supplementary interpretation of the Terms (
+          <i>ergänzende Vertragsauslegung</i>) does not have precedence or is
+          not possible.
+        </li>
+      </ol>
+    </StaticPage>
+  );
+}
