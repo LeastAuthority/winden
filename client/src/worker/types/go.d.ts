43f2147bc32070415a9fe44e2a6a20d501a06545
Justus Tumacder <justusft@gmail.com>
2022-12-01 05:15:15 +0800

    Delete worker

--- client/src/worker/types/go.d.ts
+++
@@ -1,5 +0,0 @@
-export {};
-
-declare global {
-  let Go: any // or a more specific type
-}
--- client/src/worker/wasm_exec.d.ts
+++
@@ -1,9 +0,0 @@
-// NB: tried @types/golang-wasm
-
-declare class Go {
-  constructor();
-  importObject: Record<string, Record<string, any>>;
-  run(instance: any): any;
-  exit: (code: number) => void;
-}
-export default Go;
