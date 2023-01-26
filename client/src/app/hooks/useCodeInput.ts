416da8eea715fd2f134d6fc7b5674ffb8aac5525
Justus Tumacder <justusft@gmail.com>
2023-01-26 10:20:55 +0800

    Remove code for old wormhole bindings and state management

--- client/src/app/hooks/useCodeInput.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { CodeInputContext } from "../components/providers/CodeInputProvider";
-
-export function useCodeInput() {
-  return useContext(CodeInputContext);
-}
--- client/src/app/hooks/useWormhole.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { WormholeContext } from "../components/providers/WormholeProvider";
-
-export function useWormhole() {
-  const wormhole = useContext(WormholeContext);
-  return wormhole;
-}
