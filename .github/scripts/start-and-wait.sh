29f8f6a0a2d7d953dc98c82266470d68b8dc5168
Justus Tumacder <justusft@gmail.com>
2023-04-21 17:10:59 +0800

    no silent curl

--- .github/scripts/start-and-wait.sh
+++ .github/scripts/start-and-wait.sh
@@ -4,7 +4,7 @@ docker compose --profile e2e up -d client
 printf "Waiting for webpack to compile...\n"
 while :; do
   sleep 1
-  status="$(curl -s -k "https://127.0.0.1:8080/bundle-status")" 
+  status="$(curl -k "https://127.0.0.1:8080/bundle-status")" 
   if [[ $status =~ "true" ]]; then
     break
   fi
