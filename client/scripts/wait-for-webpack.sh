9c5c482e3eff80a498572beae806e1f9f94aba21
Justus Tumacder <justusft@gmail.com>
2023-04-24 11:39:36 +0800

    Run wait script in docker container instead

--- client/scripts/wait-for-webpack.sh
+++ client/scripts/wait-for-webpack.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-printf "Starting server\n"
-docker compose --profile e2e up -d client
 printf "Waiting for webpack to compile...\n"
 while :; do
   sleep 1
-  status="$(curl -k "https://client:8080/bundle-status")" 
+  status="$(curl -k "https://localhost:8080/bundle-status")" 
   if [[ $status =~ "true" ]]; then
     break
   fi
