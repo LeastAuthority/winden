43dd42845f612795e3d68395268b97dd2634f923
Justus Tumacder <justusft@gmail.com>
2023-06-21 07:12:55 +0800

    Remove console.logs

--- client/src/app/util/downloader/cleanerWorker.ts
+++ client/src/app/util/downloader/cleanerWorker.ts
@@ -1,12 +1,10 @@
 async function cleanup() {
-  console.log("Executing cleanup");
   const directoryHandle = await navigator.storage.getDirectory();
   const now = Date.now();
   // @ts-ignore
   for await (const [filename, handle] of directoryHandle.entries()) {
     const file: File = await handle.getFile();
     if (now >= file.lastModified + 60000) {
-      console.log(`Deleting ${filename}`);
       directoryHandle.removeEntry(filename);
     }
   }
