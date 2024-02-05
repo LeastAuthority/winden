536653d17e481003fef919ee1a20c3c3d0e166e9
Benoit Donneaux <benoit@leastauthority.com>
2024-02-05 22:46:30 +0100

    Wait for webpack w/o ssl too
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- client/scripts/wait-for-webpack.sh
+++ client/scripts/wait-for-webpack.sh
@@ -3,7 +3,7 @@ printf "Waiting for webpack to compile...\n"
 while :; do
   sleep 1
   printf .
-  status="$(curl -s -k "https://localhost:8080/bundle-status")" 
+  status="$(curl -s -k "http://localhost:8080/bundle-status")" 
   if [[ $status =~ "true" ]]; then
     break
   fi
